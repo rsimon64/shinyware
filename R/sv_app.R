a4002d4c8ffcbb2b27de2aa356a1c2babaa68ca5
Reinhard Simon <rsimon64@gmail.com>
2018-05-08 16:48:40 -0500

    u

--- R/sv_app.R
+++ R/sv_app.R
@@ -1,3 +1,12 @@
 
 
 sv_app <- function(input, output, session) {}
+
+sw_app <- function(input, output, session) {
+  sv_app(input, output, session)
+  
+  session$onSessionEnded(function() {
+    shiny::stopApp() 
+  })
+  
+}
